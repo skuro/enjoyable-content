918b47f4c22f31188233de33b2c1c1fc1a760705
Carlo Sciolla <skuro@skuro.tk>
2013-02-05 15:55:50 +0100

    Added license; better docs

--- README.md
+++ README.md
@@ -1,7 +1,22 @@
 Enjoyable Content
 =================
 
-> **groov·y** • _/ˈgro͞ovē/_
+> **groov·y** • _/ˈgro͞ovē/_ •
 > _adj._
 >
 > Enjoyable and excellent.
+
+
+This is a collection of Groovy scripts intended to be used in the [OpenCMIS Workbench] to handle content.
+
+Installation
+============
+
+You can just clone this repository on your machine, then start the Groovy console from the Workbench and open the script you want to execute.
+
+Most of the scripts will need some customizations in order to be useful, e.g. by setting a path or a configuration option. Be sure to read the script carefully before you run it against your production environment, there's no refund policy!
+
+License
+=======
+
+All the code in this repository is released under the MIT license, see [LICENCE](https://github.com/skuro/enjoyable-content/blob/master/LICENSE.txt)
--- deleteChildren.groovy
+++ deleteChildren.groovy
@@ -1,7 +1,35 @@
-import org.apache.chemistry.opencmis.commons.*
-import org.apache.chemistry.opencmis.commons.data.*
-import org.apache.chemistry.opencmis.commons.enums.*
-import org.apache.chemistry.opencmis.client.api.*
+/*
+ * Copyright (C) 2013,2014 Carlo Sciolla
+ *
+ * Licensed under the MIT License ("the License")
+ * You may obtain a copy of the 
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://github.com/skuro/enjoyable-content/blob/master/LICENSE.txt
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import org.apache.chemistry.opencmis.commons.*;
+import org.apache.chemistry.opencmis.commons.data.*;
+import org.apache.chemistry.opencmis.commons.enums.*;
+import org.apache.chemistry.opencmis.client.api.*;
+
+/*
+ * Deletes all the children of a given folder. It will log all the file names that it will delete.
+ *
+ * Instructions:
+ *   - configure the path of the folder you want to empty
+ *   - run the script
+ *
+ */
+
+def path = "/Path"; // customize here
 
 def getBaseFolder(node, path) {
     session.getObjectByPath(path);
@@ -23,6 +51,6 @@ def deleteNodes(children) {
 }
 
 def root = session.getRootFolder();
-def base = getBaseFolder(root, "/Path"); // customize here
+def base = getBaseFolder(root, path);
 def children = getNodes(base);
 deleteNodes(children);
